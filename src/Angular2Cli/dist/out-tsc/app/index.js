5f9201c42e14ab0b9892da4cb2d06927a0aafbc5
saccolino <saccolino@gmail.com>
2016-11-29 14:29:08 +1100

    delete dist folder

--- src/Angular2Cli/dist/out-tsc/app/index.js
+++
@@ -1,3 +0,0 @@
-export * from './app.component';
-export * from './app.module';
-//# sourceMappingURL=D:/PoCs/Angular2Cli/src/Angular2Cli/Client/app/index.js.map
--- src/Angular2Cli/dist/out-tsc/environments/environment.prod.js
+++
@@ -1,4 +0,0 @@
-export var environment = {
-    production: true
-};
-//# sourceMappingURL=D:/PoCs/Angular2Cli/src/Angular2Cli/Client/environments/environment.prod.js.map
